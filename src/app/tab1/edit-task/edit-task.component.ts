9301753d0c42a97fae164ba098762261f63891f4
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2023-11-04 21:35:43 +0000

    Fixing the edit form

--- src/app/tab1/edit-task/edit-task.component.ts
+++ src/app/tab1/edit-task/edit-task.component.ts
@@ -4,6 +4,12 @@ import { firstValueFrom } from 'rxjs';
 import { BacklogItemResponseDto } from '../dtos/backlog-item.dto';
 import { EditTaskService } from './edit-task.service';
 
+interface EditForm {
+  name: string;
+  description: string;
+  points: number;
+}
+
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
@@ -11,12 +17,11 @@ import { EditTaskService } from './edit-task.service';
 })
 export class EditTaskComponent implements OnInit {
   name: string = 'hello';
-  backlogItem: BacklogItemResponseDto = {
-    id: '',
+
+  form: EditForm = {
     name: '',
     description: '',
     points: 0,
-    label: '',
   };
 
   submitted = false;
@@ -30,9 +35,13 @@ export class EditTaskComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     console.log(this.itemId);
-    this.backlogItem = await firstValueFrom(
+    const result = await firstValueFrom(
       this.service.getBacklogItem(this.itemId)
     );
+
+    this.form.description = result.description;
+    this.form.name = result.name;
+    this.form.points = result.points;
   }
 
   cancel() {
@@ -41,11 +50,14 @@ export class EditTaskComponent implements OnInit {
 
   async onSubmit() {
     console.log('form submitted');
-    console.debug(this.backlogItem);
+    console.debug(this.form);
     try {
-      this.backlogItem = await firstValueFrom(
-        this.service.editBacklogItem(this.itemId, this.backlogItem)
+      const result = await firstValueFrom(
+        this.service.editBacklogItem(this.itemId, this.form)
       );
+      this.form.description = result.description;
+      this.form.name = result.name;
+      this.form.points = result.points;
     } catch (err) {
       console.error(err);
     }
